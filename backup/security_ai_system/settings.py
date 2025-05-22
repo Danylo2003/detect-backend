bb6c7df9f02f423a58e3aaaab0e595e7e543068a
Danylo2003 <danylo.karboinov.dev@gmail.com>
2025-05-23 00:39:44 +0300

    Fixed some issue

--- backup/security_ai_system/settings.py
+++ backup/security_ai_system/settings.py
@@ -148,8 +148,8 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/drf-yasg/')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = '/media/'
